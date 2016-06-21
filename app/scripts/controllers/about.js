aa47f3995a5eb33a666014e19a997e2e0596ffef
annant.gupta <annant.gupta@routerabbit.com>
2016-06-21 18:23:54 +0530

    project created

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name drestClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the drestClientApp
+ */
+angular.module('drestClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name drestClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the drestClientApp
+ */
+angular.module('drestClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
